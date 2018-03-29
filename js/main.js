a2f26cb1de978406237f77356fd7d89283d7c9bf
Unknown <damienvignes@hotmail.com>
2018-03-29 16:37:29 +0200

    Finish section races

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
 	$('#fullpage').fullpage();
 });
+
+
+$(document).ready(function(){
+  $('.bxslider').bxSlider({
+
+      slideWidth: 292.5,
+      auto: true,
+      minSlides: 1,
+      maxSlides: 3,
+      slideMargin: 50
+  });
+});
+
+$(document).ready(function() {
+   'use strict';
+   new WOW().init();
+
+ })
